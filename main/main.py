751952356de117ad3849954b33d2ef0abfa38d70
Aayush Arya <tomriddle257@gmail.com>
2020-06-17 17:27:40 +0530

    Rewrite clean_taut method to fix bugs and improve efficiency

--- main/main.py
+++ main/main.py
@@ -4,31 +4,117 @@ from rdkit.Chem.MolStandardize import rdMolStandardize
 from molvs import tautomer
 
 include(os.path.abspath(os.path.join('..', 'rules/all.py')))
-postChapter('Alkaline Glucose Degradation')
+#postChapter('Alkaline Glucose Degradation')
 
 # starting molecule
+#some_molecule = smiles('NCC=CO', name = 'starting molecule')
+#some_molecule.print()
 glucose = smiles('OC[C@H]1O[C@H](O)[C@H](O)[C@@H](O)[C@@H]1O', name='Glucose')
 
 # Forbidden substructures
 # Three and four membeered rings are unstable
-forbidden = [smiles('C1CC1', name='cyclopropane'), smiles('C1CCC1', name = 'cyclobutane')]
+oxirane = graphGMLString("""graph [
+   node [ id 0 label "C" ]
+   node [ id 1 label "C" ]
+   node [ id 2 label "O" ]
+   edge [ source 0 target 1 label "-"]
+   edge [ source 0 target 2 label "-"]
+   edge [ source 1 target 2 label "-"]
+]""", name='Oxirane')
+
+aziridine = graphGMLString("""graph [
+   node [ id 0 label "C" ]
+   node [ id 1 label "C" ]
+   node [ id 2 label "N" ]
+   edge [ source 0 target 1 label "-"]
+   edge [ source 0 target 2 label "-"]
+   edge [ source 1 target 2 label "-"]
+]""", name='aziridine')
+forbidden = [smiles('C1CC1', name='cyclopropane'), smiles('C1CCC1', name = 'cyclobutane'),
+            oxirane, aziridine]
 
 # make sure these don't get passed as an input
 for fb in forbidden:
     inputGraphs.remove(fb)
-
 strat = (
     addSubset(inputGraphs)
     >> rightPredicate[
         lambda derivation: all (g.exactMass <= 500
          and (g.monomorphism(fb) == 0 for fb in forbidden) for g in derivation.right)
-    ] (repeat[1](inputRules))
+    ] (inputRules)#repeat[1]()
 )
 
 # molVs smiles might not be identical to rdkit so let's use RDKit instead of molVs
 enum = rdMolStandardize.TautomerEnumerator()
 #enum = tautomer.TautomerEnumerator()
 
+def clean_taut(dg, dg_execute):
+    subset = dg_execute.subset
+    universe = dg_execute.universe
+    # The following line may seem redundant but it really isn't
+    graphs = [g for g in subset]
+    mod_subset_smiles = [g.smiles for g in subset]
+    mod_dgverts = {v.graph: v for v in dg.vertices}
+    rdkit_subset_smiles = []
+    #vertex_dg_vertex = {v.graph: v for v in dg.vertices if v.graph in subset}
+
+    taut_tautset_dict = {}
+    to_remove = set()
+    # Populate all possible tautomers and add to the above empty dict
+    for mod_smiles in mod_subset_smiles:
+        mol = Chem.MolFromSmiles(mod_smiles)
+        # convert into rdkit canonical smiles
+        rdkit_smiles = Chem.MolToSmiles(mol)
+        rdkit_subset_smiles.append(rdkit_smiles)
+        # generate Mol objects of all possible tautomers
+        all_tauts = enum.Enumerate(mol)
+        # convert into smiles
+        all_taut_smiles = tuple(Chem.MolToSmiles(taut) for taut in all_tauts)
+        #print(all_taut_smiles)
+        taut_tautset_dict[mod_smiles] = all_taut_smiles
+
+    list_tautset = tuple(taut_tautset_dict.values())
+    class_ids = {}
+    # create initial class ids
+    for i in range(len(list_tautset)):
+        class_ids.update({list_tautset[i]: i})
+
+    # Make common taut classes
+    # This is worse than O(n^2) but let's see how it goes
+    for i in range(len(list_tautset)):
+        for j in range(i+1, len(list_tautset)):
+            for item in list_tautset[i]:
+                if item in list_tautset[j]:
+                    #print(f'Classes {i} and {j} have common elements')
+                    #class_ids[list_tautset[i]] = class_ids[list_tautset[i]]
+                    class_ids[list_tautset[j]] = class_ids[list_tautset[i]]
+                    #print('Updated class ids to ', class_ids[list_tautset[i]])
+                    break
+    # Now, since we're done finding identical class ids
+    dict_observed_tauts = {}
+    for taut_class, class_id in class_ids.items():
+        for i in range(len(rdkit_subset_smiles)):
+            if rdkit_subset_smiles[i] in taut_class:
+                # the smiles of the mod graph
+                if class_id in dict_observed_tauts.keys():
+                    dict_observed_tauts[class_id].append(mod_subset_smiles[i])
+                else :
+                    dict_observed_tauts[class_id] = [mod_subset_smiles[i]]
+    print('{0} unique tautomer classes in {1} molecules'.format(len(dict_observed_tauts.keys())
+                                , len(subset)))
+    for taut_class in dict_observed_tauts.values():
+        if len(taut_class) > 1:
+            for i in range(1, len(taut_class)):
+                to_remove.add(taut_class[i])
+    for item in to_remove:
+        subset.remove(graphs[mod_subset_smiles.index(item)])
+        universe.remove(graphs[mod_subset_smiles.index(item)])          
+    #for i in range(len(rdkit_subset_smiles)):
+
+    # Merge tautomer classes
+    # Now score each tautomer in each class
+
+    return subset, universe
 # What if none of the existing tautomers is the most stable one?
 # We keep the most stable out of the ones in the network since we can't modify graphs in MOD
 # enumerate all possible tautomers for each molecule
@@ -38,7 +124,7 @@ enum = rdMolStandardize.TautomerEnumerator()
 # return the cleaned tautomers once done.
 
 # The code became really ugly while optimizing for performance using dictionaries
-def clean_taut(dg, dg_execute):
+'''def clean_taut(dg, dg_execute):
     subset = dg_execute.subset
     universe = dg_execute.universe
 
@@ -74,6 +160,14 @@ def clean_taut(dg, dg_execute):
             taut_pair_dict[taut_list].append(smiles)
         else:
             taut_pair_dict.update({taut_list: [smiles]})
+    # Debugging this region
+    f = open('debug.txt', 'w')
+    for i in range(len(taut_pair_dict.keys())-1):
+        for j in range(1,len(taut_pair_dict.keys())):
+            for item in list(taut_pair_dict.keys())[i]:
+                if item in list(taut_pair_dict.keys())[j] and list(taut_pair_dict.keys())[i] != list(taut_pair_dict.keys())[j]:
+                    #f.write('An item in {0} was found in {1}\n'.format(list(taut_pair_dict.keys())[i], list(taut_pair_dict.keys())[j]))
+    f.close()
     # The following part has complexity O(n^2)
     # TODO: try to see if you can turn this into O(n logn)
     # This only removes the tautomers (all except the most stable one in the graph)
@@ -104,23 +198,39 @@ def clean_taut(dg, dg_execute):
             subset.remove(graph)
             universe.remove(graph)
             print(f"Removed {graph} from subset and universe")
-    return subset, universe
+    return subset, universe'''
 
 # Number of generations we want to perform
-generations = 2
+generations = 3
 
+postSection('Final Network')
 dg = DG(graphDatabase=inputGraphs)
 with dg.build() as b:
     res = b.execute(strat, verbosity=2, ignoreRuleLabelTypes=True)
     subset, universe = clean_taut(dg, res)
     for gen in range(generations-1):
-        print(f'Graph has total {dg.numVertices} vertices')
         res = b.execute(addSubset(subset) >> addUniverse(universe) >> strat,
                             verbosity=2, ignoreRuleLabelTypes=True)
         print('Original subset size:', len(res.subset))
         subset, universe = clean_taut(dg, res)
-        print('Size after removal:', len(subset))
+        print('Subset size after removal:', len(subset))
         # This step replaces the previous subset (containing tautomers) with the cleaned subset
         res = b.execute(addSubset(subset) >> addUniverse(universe))
     print('Completed')
 #dg.print()
+postSection('Individual Vertices')
+p = GraphPrinter()
+p.simpleCarbons = True
+p.withColour = True
+p.collapseHydrogens = True
+'''for fb in forbidden:
+    fb.print()
+for v in dg.vertices:
+    for item in forbidden:
+        if v.graph.monomorphism(item) == 1:
+            print(f'Found substructure {item.name} in {v.graph.name}')'''
+'''for v in dg.vertices:
+    v.graph.print(p)
+postSection('Individual Edges')
+for e in dg.edges:
+    e.print()'''
