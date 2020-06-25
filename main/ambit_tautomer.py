639836306f7fc9183cfbe8622c356a8300f9db1b
Aayush Arya <tomriddle257@gmail.com>
2020-06-26 00:55:43 +0530

    Fixed everything, can generate using Ambit now

--- main/ambit_tautomer.py
+++ main/ambit_tautomer.py
@@ -3,42 +3,48 @@ import jpype.imports
 from jpype.types import *
 
 
-jpype.startJVM(classpath=['libs/ambit-tautomers-2.0.0-SNAPSHOT.jar', 'libs/cdk-2.3.jar', 'libs/custom_utils.jar'],
+jpype.startJVM(classpath=['libs/ambit-tautomers-2.0.0-SNAPSHOT.jar', 'libs/cdk-2.3.jar'],
                 convertStrings=False)
 
 java = jpype.JPackage("java")
 ambit2 = jpype.JPackage("ambit2")
 cdk = jpype.JPackage("org").openscience.cdk
 
-tautomerManager = ambit2.tautomers.TautomerManager
-#AmbitUtils = jpype.JPackage("kt").AmbitUtilsKt
-#AmbitUtils.generateTautomers("CC=O","IA-DFS")
-chemObjectBuilder = cdk.silent.SilentChemObjectBuilder.getInstance()
-try:
-    smilesParser = cdk.smiles.SmilesParser(chemObjectBuilder)
-    mol = smilesParser.parseSmiles('CC=O')
-except cdk.exception.InvalidSmilesException as e:
-    e.printStackTrace()
+def generateTautomers(smiles, mode="IA-DFS"):
+    """
+    Generate the list of possible tautomers for a given molecule
 
-# It keeps inferring <java org.openscience.cdk.silent.Molecule> as the type of the object
-# created above. It needs to be <java org.openscience.cdk.interfaces.IAtomContainer>
-# This problem doesn't arise when I try to do this in Kotlin/Java
-print(type(mol))
+    Keyword arguments:
+    smiles -- the SMILES string of the molecule
+    mode -- The generation algorithm to use (default: "IA-DFS")
 
-# The following will now throw an exception due to the above bad type inference.
-try :
+    Available  algorithms include:
+        "CM" for Simple Combinatorial,
+        "CMI" for Improved Combinatorial,
+        "IA-DFS" for Incremental Algorithm - Depth First Search
+        "combined" for a combination of CMI and IA-DFS
+    See https://onlinelibrary.wiley.com/doi/abs/10.1002/minf.201200133 for a detailed discussion
+        of the algorithms
+
+    Returns: a list of SMILES strings of the possible tautomers
+    """
+    tautomerManager = ambit2.tautomers.TautomerManager()
+    silentChemObjectBuilder = cdk.silent.SilentChemObjectBuilder.getInstance()
+    try:
+        smilesParser = cdk.smiles.SmilesParser(silentChemObjectBuilder)
+        mol = cdk.interfaces.IAtomContainer
+        mol = smilesParser.parseSmiles('CC=O')
+    except cdk.exception.CDKException as e:
+        e.printStackTrace()
+
     tautomerManager.setStructure(mol)
-    tautomers = tautomerManager.generateTautomersIncrementally()
-except java.lang.Exception as e:
-    e.printStackTrace()
-
-
-# Perhaps we could use case labels to decide which algorithm to use.
-# (Not) using these constants will not affect code execution. These are used just for reference
-#GAT = ambit2.tautomers.TautomerConst.GAT
-#print(type(jpype.JClass(ambit2.tautomers.TautomerConst).GAT))
-# available algorithms: GAT.Comb_Pure, GAT.Comb_Improved, GAT.Incremental
-#algorithm = GAT.Incremental
-
-#rank = TautomerConst.TAUTOMER_RANK
-#System.out.println(rank)
+    if mode == "IA-DFS":
+        tautomers = tautomerManager.generateTautomersIncrementaly()
+    elif mode == "combined":
+        tautomers = tautomerManager.generateTautomersCombinedApproach()
+    elif mode == "CMI":
+        tautomers = tautomerManager.generateTautomers_ImprovedCombApproach()
+    elif mode == "CM":
+        tautomers = tautomerManager.generateTautomers()
+    smilesGenerator = cdk.smiles.SmilesGenerator(True)
+    return [smilesGenerator.createSMILES(taut) for taut in tautomers]
