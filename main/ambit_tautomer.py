0abf5e3ce28aaae3ab722872e4b993678327d5e4
Aayush Arya <tomriddle257@gmail.com>
2020-06-25 19:25:57 +0530

    Some progress with JPype

--- main/ambit_tautomer.py
+++ main/ambit_tautomer.py
@@ -1,41 +1,37 @@
-import os
 import jpype
 import jpype.imports
-from jpype import JImplements, JOverride
 from jpype.types import *
 
-jpype.startJVM(classpath=['libs/ambit-tautomers-2.0.0-SNAPSHOT.jar', 'libs/cdk-2.3.jar'], 
+
+jpype.startJVM(classpath=['libs/ambit-tautomers-2.0.0-SNAPSHOT.jar', 'libs/cdk-2.3.jar', 'libs/custom_utils.jar'],
                 convertStrings=False)
-from java.lang import System
 
-# I was printing the classpath for testing
-#print(System.getProperty("java.class.path"))
-
+java = jpype.JPackage("java")
 ambit2 = jpype.JPackage("ambit2")
 cdk = jpype.JPackage("org").openscience.cdk
 
-tautomerManager = ambit2.tautomers.TautomerManager()
+tautomerManager = ambit2.tautomers.TautomerManager
+#AmbitUtils = jpype.JPackage("kt").AmbitUtilsKt
+#AmbitUtils.generateTautomers("CC=O","IA-DFS")
+chemObjectBuilder = cdk.silent.SilentChemObjectBuilder.getInstance()
+try:
+    smilesParser = cdk.smiles.SmilesParser(chemObjectBuilder)
+    mol = smilesParser.parseSmiles('CC=O')
+except cdk.exception.InvalidSmilesException as e:
+    e.printStackTrace()
 
-# If you're wondering why I did this, see Using_Ambit.md (Java Interfaces can't be instantiated)
-@JImplements(cdk.interfaces.IChemObjectBuilder)
-class PythonCDKObjectBuilder:
-    def __init__(self):
-        # I don't know if anything else needs to be done but this compiles fine
-        pass
-    # Must override Interface method "newInstance"
-    @JOverride
-    def newInstance(object):
-        return self
-objectBuilder = PythonCDKObjectBuilder()
-smilesParser = cdk.smiles.SmilesParser(objectBuilder)
+# It keeps inferring <java org.openscience.cdk.silent.Molecule> as the type of the object
+# created above. It needs to be <java org.openscience.cdk.interfaces.IAtomContainer>
+# This problem doesn't arise when I try to do this in Kotlin/Java
+print(type(mol))
 
-# TODO: fix this
-# The following line throws the seemingly unrelated error
-# TypeError: newInstance() takes 1 positional argument but 3 were given
-mol = smilesParser.parseSmiles('CC=O')
+# The following will now throw an exception due to the above bad type inference.
+try :
+    tautomerManager.setStructure(mol)
+    tautomers = tautomerManager.generateTautomersIncrementally()
+except java.lang.Exception as e:
+    e.printStackTrace()
 
-#tautomerManager.setStructure(mol)
-#tautomerManager.generateTautomersIncrementaly
 
 # Perhaps we could use case labels to decide which algorithm to use.
 # (Not) using these constants will not affect code execution. These are used just for reference
