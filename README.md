6198e4c1d2d6722be54fb25710efcb650d0c1e42
Aayush Arya <tomriddle257@gmail.com>
2020-07-11 12:53:16 +0530

    Use open chain glucose

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 * Fix the reaction rules that aren't currently invertible for some reason.
     * Check the ones with wildcards.
 * Write more reaction rules (to account for missing products).
-* Compare with mass spectra at the end of each generation and see what % of masses are matching.
-    * Perhaps represent this info graphically.
-* See which step in the tautomer cleaning process is the most inefficient.
+* ~~Compare with mass spectra at the end of each generation and see what % of masses are matching.~~
+    * ~~Perhaps represent this info graphically.~~
+* ~~See which step in the tautomer cleaning process is the most inefficient.~~
+    * The slowest part is enumerating all possible tautomers for the given molecules. Takes more time to enumerate the possibilties than it does to produce the molecules themselves.
--- main/main.py
+++ main/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import compare_ms
 from rdkit.Chem import MolFromSmiles, MolToSmiles
 
@@ -8,7 +9,8 @@ include('clean_tautomers.py')
 postChapter('Alkaline Glucose Degradation')
 
 # starting molecule
-glucose = smiles('OC[C@H]1O[C@H](O)[C@H](O)[C@@H](O)[C@@H]1O', name='Glucose')
+#glucose = smiles('OC[C@H]1O[C@H](O)[C@H](O)[C@@H](O)[C@@H]1O', name='Glucose')
+open_glucose = smiles("O=C[C@H](O)[C@H](O)[C@H](O)[C@H](O)[C@@H](O)")
 
 # Forbidden substructures
 # Three and four membeered rings are unstable, C=C=C is forbidden
@@ -27,12 +29,13 @@ def pred(derivation):
     """
     for g in derivation.right:
         # Allow masses > 500 for
-        #if g.exactMass >= 500:
-        #    return False
+        if g.exactMass >= 500:
+            return False
         for fb in forbidden:
             if fb.monomorphism(g) > 0:
-                #print(f"Found {fb} in {g}")
+                print(f"Found {fb} in {g}")
                 return False
+        print(g)
     return True
 
 
@@ -46,35 +49,41 @@ strat = (
 )
 
 # Number of generations we want to perform
-generations = 3
+generations = 2
 
 postSection('Final Network')
 dg = DG(graphDatabase=inputGraphs)
 
-subset = universe = inputGraphs
+subset = inputGraphs
+universe = []
 with dg.build() as b:
     for gen in range(generations):
+        start_time = time.time()
+        print(f"Starting round {gen+1}")
         res = b.execute(addSubset(subset) >> addUniverse(universe) >> strat,
                             verbosity=2, ignoreRuleLabelTypes=True)
+        end_time = time.time()
+        print(f"Took {end_time - start_time} seconds to complete round {gen+1}")
         print('Original subset size:', len(res.subset))
 
+        # The returned subset and universe do not contain redundant tautomers
         #subset, universe = clean_taut(dg, res)
         subset, universe = res.subset, res.universe
-        print('Subset size after removal:', len(subset))
+        #print('Subset size after removal:', len(subset))
         # This step replaces the previous subset (containing tautomers) with the cleaned subset
         #res = b.execute(addSubset(subset) >> addUniverse(universe))
         # now compare how many of these simulations were found in the MS data.
-        compare_ms.compare_sims([v.graph.smiles for v in dg.vertices], gen+1)
+        #compare_ms.compare_sims([v.graph.smiles for v in dg.vertices], gen+1)
     print('Completed')
 # Make a mass spectra (a histogram of the masses) of the molecules
-compare_ms.make_mass_spectra([v.graph.smiles for v in dg.vertices])
+#compare_ms.make_mass_spectra([v.graph.smiles for v in dg.vertices])
 #dg.print()
-postSection('Individual Vertices')
+'''postSection('Individual Vertices')
 p = GraphPrinter()
 p.simpleCarbons = True
 p.withColour = True
 p.collapseHydrogens = True
-'''
+
 for v in dg.vertices:
     v.graph.print(p)
 postSection('Individual Edges')
