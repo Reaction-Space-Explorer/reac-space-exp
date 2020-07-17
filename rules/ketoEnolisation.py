c74dbf918441f2c84e52236388e770dea6cbedb9
Aayush Arya <tomriddle257@gmail.com>
2020-07-17 23:09:11 +0530

    Do keto-enol migration twice in one step

--- rules/ketoEnolisation.py
+++ rules/ketoEnolisation.py
@@ -1,20 +1,21 @@
 ketoEnolisation = [ruleGMLString("""rule [
-    ruleID "ketoEnolisation"
+    ruleID "Keto-enol migration twice" # to avoid enols (and tautomers in general)
     left[
         edge [ source 1 target 2 label "=" ]
-        edge [ source 1 target 3 label "-" ]
         edge [ source 3 target 4 label "-" ]
     ]
     context [
         node [ id 1 label "C" ]
         node [ id 2 label "O" ]
         node [ id 3 label "C" ]
-        node [ id 4 label "H" ]
+        node [ id 4 label "O" ]
+        node [ id 5 label "H" ]
+        edge [ source 4 target 5 label "-" ]
+        edge [ source 1 target 3 label "-" ]
     ]
     right [
-        edge [ source 1 target 3 label "=" ]
-        edge [ source 1 target 2 label "-" ]
-        edge [ source 2 target 4 label "-" ]
+        edge [ source 1 target 4 label "-" ]
+        edge [ source 3 target 2 label "=" ]
     ]
 ]
 """)]
