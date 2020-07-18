043677486460cf64d510141e6246ac1ab5800790
Aayush Arya <tomriddle257@gmail.com>
2020-07-19 03:57:59 +0530

    Add rules for hydration (inverse of elimination)

--- rules/hydration.py
+++ rules/hydration.py
@@ -1,7 +1,45 @@
 # The purpose of this is to provide an "inverse" rule to elimination
 
+# Hydration + 
 hydration1 = [ruleGMLString("""rule [
-    ruleID "Hydration"
+    ruleID "Hydration of C(=O)C"
+    left [
+        edge [ source 1 target 2 label "=" ]
+        edge [ source 3 target 4 label "-" ]
+        edge [ source 5 target 6 label "-" ]
+    ]
+    context [
+        node [ id 1 label "C" ]
+        node [ id 2 label "O" ]
+        node [ id 3 label "C" ]
+        node [ id 4 label "H" ]
+        #H2O
+        node [ id 5 label "O" ]
+        node [ id 6 label "H" ]
+        node [ id 7 label "H" ]
+
+        edge [ source 1 target 3 label "-" ]
+        edge [ source 5 target 7 label "-" ]
+    ]
+    right [
+        edge [ source 1 target 2 label "-" ]
+        edge [ source 1 target 4 label "-" ]
+        edge [ source 2 target 6 label "-" ]
+        edge [ source 5 target 3 label "-" ]
+    ]
+    constrainAdj [ id 1 op "=" count 0 
+        edgeLabels [ label "-" ]
+        nodeLabels [ label "N" label "S" label "O" ]
+    ]
+    constrainAdj [ id 3 op "=" count 0
+        edgeLabels [ label "=" label "-" ] # avoid gem diols as well
+        nodeLabels [ label "O" label "N" label "S" ]
+    ]
+]
+""")]
+
+hydration2 = [ruleGMLString("""rule [
+    ruleID "Hydration of C=C(O)"
     left [
         edge [ source 1 target 2 label "=" ]
         edge [ source 3 target 4 label "-" ]
@@ -28,20 +66,6 @@ hydration1 = [ruleGMLString("""rule [
     constrainAdj [ id 2 op "=" count 0
         nodeLabels [ label "O" ]
         edgeLabels [ label "=" ]
-    ]
-]
-""")]
-
-# Hydration + 
-hydration2 = [ruleGMLString("""rule [
-    left [
-
-    ]
-    context [
-
-    ]
-    right [
-        
     ]
 ]
 """)]
