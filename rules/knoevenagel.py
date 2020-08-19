21a05359b5d44b9ddcbd791b3346079a4cf622ed
Aayush Arya <tomriddle257@gmail.com>
2020-08-20 00:57:09 +0530

    Condense knoevenagel into just 2 rules

--- rules/knoevenagel.py
+++ rules/knoevenagel.py
@@ -1,6 +1,89 @@
 include("common.py")
 
-def knoevenagelGen():
+knoevenagel_c = [ruleGMLString("""rule [
+	ruleID "Knoevenagel C"
+	labelType "term"
+	left [
+		edge [ source 1 target 2 label "=" ]
+		edge [ source 3 target 4 label "-" ]
+	]
+	context [
+		node [ id 1 label "C" ]
+		node [ id 2 label "O" ]
+
+		node [ id 3 label "C" ]
+		node [ id 4 label "H" ]
+		node [ id 5 label "*" ]
+		node [ id 6 label "C" ]
+		node [ id 7 label "C" ]
+
+		edge [ source 3 target 6 label "-" ]
+		edge [ source 3 target 7 label "-" ]
+		edge [ source 3 target 5 label "-" ]
+	]
+	right [
+		edge [ source 1 target 3 label "-" ]
+		edge [ source 1 target 2 label "-" ]
+		edge [ source 2 target 4 label "-" ]
+	]
+	# The C=O that merges should not be a part of a -(C=O)NH2, etc.
+	constrainAdj [ id 1 op "=" count 0
+		nodeLabels [ label "O" label "N" label "S" ]
+		edgeLabels [ label "-" ]
+	]
+	# The R can be either C#N or C=O
+	constrainAdj [ id 6 op "=" count 1
+		nodeLabels [ label "O" label "N" ]
+		edgeLabels [ label "=" label "#" ] # Warning: this may allow =N to be matched as well
+	]
+	constrainAdj [ id 7 op ">=" count 1
+		nodeLabels [ label "O" label "N" ]
+		edgeLabels [ label "-" label "=" label "#" ] # Warning: this may allow =N to be matched as well
+	]
+]""")]
+
+knoevenagel_h = [ruleGMLString("""rule [
+	ruleID "Knoevenagel H"
+	labelType "term"
+	left [
+		edge [ source 1 target 2 label "=" ]
+		edge [ source 3 target 4 label "-" ]
+	]
+	context [
+		node [ id 1 label "C" ]
+		node [ id 2 label "O" ]
+
+		node [ id 3 label "C" ]
+		node [ id 4 label "H" ]
+		node [ id 5 label "*" ]
+		node [ id 6 label "C" ]
+
+		edge [ source 3 target 6 label "-" ]
+		edge [ source 3 target 5 label "-" ]
+	]
+	right [
+		edge [ source 1 target 3 label "-" ]
+		edge [ source 1 target 2 label "-" ]
+		edge [ source 2 target 4 label "-" ]
+	]
+	# The C=O that merges should not be a part of a -(C=O)NH2, etc.
+	constrainAdj [ id 1 op "=" count 0
+		nodeLabels [ label "O" label "N" label "S" ]
+		edgeLabels [ label "-" ]
+	]
+	# The R can be either C#N or C=O
+	constrainAdj [ id 6 op "=" count 1
+		nodeLabels [ label "O" label "N" ]
+		edgeLabels [ label "=" label "#" ] # Warning: this may allow =N to be matched as well
+	]
+	constrainAdj [ id 3 op ">=" count 1
+		nodeLabels [ label "O" label "N" ]
+		edgeLabels [ label "-" label "=" label "#" ] # Warning: this may allow =N to be matched as well
+	]
+]""")]
+
+
+'''def knoevenagelGen():
 	def attach_CN_CdOA(r, root, offset):
 		r.context.extend([
 			"# CN or C(=O)A",
@@ -55,4 +138,4 @@ def knoevenagelGen():
 				for r in attach_2_H_C(r, 10, 400):
 					yield r.loadRule()
 
-knoevenagel = [a for a in knoevenagelGen()]
+knoevenagel = [a for a in knoevenagelGen()]'''
